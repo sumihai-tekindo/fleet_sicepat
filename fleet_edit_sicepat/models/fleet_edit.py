fc3250a1cc1933a768bdac233625cf344171ffa6
andreanw96 <andreanw96@gmail.com>
2017-06-16 14:30:52 +0700

    Update Fleet sicepat

--- fleet_edit_sicepat/models/fleet_edit.py
+++ fleet_edit_sicepat/models/fleet_edit.py
@@ -24,7 +24,40 @@ class fleet_vehicle(osv.osv):
 		'transmission': fields.selection([('manual', 'Manual'), ('automatic', 'Automatic')], 'Transmission', help='Transmission Used by the vehicle',required=True),
 		'fuel_type': fields.selection([('gasoline', 'Gasoline'), ('diesel', 'Diesel'), ('electric', 'Electric'), ('hybrid', 'Hybrid')], 'Fuel Type', help='Fuel Used by the vehicle',required=True),
 		'tools_ids': fields.one2many('vehicle.fleet.tools','fleet_id',"Tools"),
+	}
 
-		
 
+class fleet_vehicle_log_contract(osv.osv):
+	_inherit = 'fleet.vehicle.log.contract'
+
+	def _computedate(self,cr,uid,ids,name,args,context=None):
+		res = {}
+		for fleet in self.browse(cr,uid,ids):
+			res[fleet['id']]=(datetime.date.today()+relativedelta(months=1)).strftime('%Y-%m-%d')
+		return res
+
+	_columns = {
+		'state': fields.selection([('open', 'In Progress'), ('completed','Document Completed'), ('toclose','To Close'), ('closed', 'Terminated')],
+									'Status', readonly=True, help='Choose wheter the contract is still valid or not',
+									copy=False),
+		'date_computation': fields.function(_computedate,string='Computation',type="date"),
+	}
+
+	def document_complete(self,cr,uid,ids,context=None):
+		return self.write(cr, uid, ids, {'state': 'completed'}, context=context)
+
+
+
+
+
+
+class fleet_vehicle_log_fuel(osv.osv):
+	_inherit = 'fleet.vehicle.log.fuel'
+
+
+	_columns = {
+		'start_odometer': fields.float('Odometer Sekarang'),
+		'last_odometer': fields.float('Odometer Terakhir'),
+		'driver_id': fields.many2one('res.partner', 'Nama Supir', help='Driver of the vehicle'),
+		'odometer_unit1': fields.related('vehicle_id', 'odometer_unit', type="char", string="Unit", readonly=True),
 	}
